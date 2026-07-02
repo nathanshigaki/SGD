c3dd6b7c445b08d67c125825388ca5aae9262071
Nathan <nathanshigaki@gmail.com>
2026-07-02 09:10:21 -0400

    feat(documentos): implementa teste de uso com Pageable no service

--- src/main/java/com/govmt/sgd/service/DocumentoService.java
+++ src/main/java/com/govmt/sgd/service/DocumentoService.java
@@ -3,6 +3,8 @@ package com.govmt.sgd.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,6 +38,12 @@ public class DocumentoService {
                 .stream()
                 .map(documentoMapper::toResponseFromDocumento)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<DocumentoResponse> getAll(Pageable pageable){
+        Page<Documento> documentos = documentoRepository.findAll(pageable);
+        return documentos.map(documentoMapper::toResponseFromDocumento);
     }
 
     @Transactional(readOnly = true)
